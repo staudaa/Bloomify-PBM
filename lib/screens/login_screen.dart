a71704b441a7ff9488e2a68b3c74b3176d19a904
Siti Raudatul Jannah <114587521+staudaa@users.noreply.github.com>
2026-05-14 19:54:11 +0700

    fixed bug

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -8,31 +8,22 @@ class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
 
   @override
-  State<LoginScreen> createState() =>
-      _LoginScreenState();
+  State<LoginScreen> createState() => _LoginScreenState();
 }
 
-class _LoginScreenState
-    extends State<LoginScreen> {
+class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController usernameController = TextEditingController();
 
-  final TextEditingController
-      usernameController =
-      TextEditingController();
-
-  final TextEditingController
-      passwordController =
-      TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
 
   bool isLoading = false;
 
   Future<void> login() async {
-
     setState(() {
       isLoading = true;
     });
 
-    String? token =
-        await ApiService.login(
+    String? token = await ApiService.login(
       usernameController.text,
       passwordController.text,
     );
@@ -42,121 +33,75 @@ class _LoginScreenState
     });
 
     if (token != null) {
-
-      await StorageService.saveToken(
-        token,
-      );
+      await StorageService.saveToken(token);
 
       if (!mounted) return;
 
       Navigator.pushReplacement(
         context,
-        MaterialPageRoute(
-          builder:
-              (context) =>
-                  const HomeScreen(),
-        ),
+        MaterialPageRoute(builder: (context) => const HomeScreen()),
       );
-
     } else {
-
       if (!mounted) return;
 
-      ScaffoldMessenger.of(context)
-          .showSnackBar(
-        const SnackBar(
-          content:
-              Text('Login gagal'),
-        ),
-      );
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(const SnackBar(content: Text('Login gagal')));
     }
   }
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-
       body: Container(
-
         decoration: const BoxDecoration(
-
           gradient: LinearGradient(
-
             begin: Alignment.topLeft,
             end: Alignment.bottomRight,
 
-            colors: [
-
-              Color(0xffF8D7E8),
-              Color(0xffEBDCF8),
-              Color(0xffDCCEF9),
-            ],
+            colors: [Color(0xffF8D7E8), Color(0xffEBDCF8), Color(0xffDCCEF9)],
           ),
         ),
 
         child: Center(
-
           child: SingleChildScrollView(
-
-            padding:
-                const EdgeInsets.all(25),
+            padding: const EdgeInsets.all(25),
 
             child: Container(
-
-              padding:
-                  const EdgeInsets.all(25),
+              padding: const EdgeInsets.all(25),
 
               decoration: BoxDecoration(
+                color: Colors.white.withValues(alpha: 0.9),
 
-                color:
-                    Colors.white.withOpacity(
-                  0.9,
-                ),
-
-                borderRadius:
-                    BorderRadius.circular(
-                  30,
-                ),
+                borderRadius: BorderRadius.circular(30),
 
                 boxShadow: [
-
                   BoxShadow(
-                    color:
-                        Colors.black12,
+                    color: Colors.black12,
 
                     blurRadius: 15,
 
-                    offset:
-                        const Offset(0, 5),
+                    offset: const Offset(0, 5),
                   ),
                 ],
               ),
 
               child: Column(
                 children: [
-
                   Container(
                     width: 100,
                     height: 100,
 
                     decoration: BoxDecoration(
-                      color:
-                          const Color(
-                        0xffF8D7E8,
-                      ),
+                      color: const Color(0xffF8D7E8),
 
-                      borderRadius:
-                          BorderRadius.circular(
-                        30,
-                      ),
+                      borderRadius: BorderRadius.circular(30),
                     ),
 
                     child: const Icon(
                       Icons.local_florist,
                       size: 60,
-                      color:
-                          Color(0xffBFA2DB),
+                      color: Color(0xffBFA2DB),
                     ),
                   ),
 
@@ -167,11 +112,9 @@ class _LoginScreenState
 
                     style: TextStyle(
                       fontSize: 35,
-                      fontWeight:
-                          FontWeight.bold,
+                      fontWeight: FontWeight.bold,
 
-                      color:
-                          Color(0xff5E548E),
+                      color: Color(0xff5E548E),
                     ),
                   ),
 
@@ -180,49 +123,30 @@ class _LoginScreenState
                   const Text(
                     'Flower Catalog App',
 
-                    style: TextStyle(
-                      color:
-                          Colors.black54,
-                    ),
+                    style: TextStyle(color: Colors.black54),
                   ),
 
                   const SizedBox(height: 40),
 
                   TextField(
-                    controller:
-                        usernameController,
+                    controller: usernameController,
 
-                    decoration:
-                        InputDecoration(
-
+                    decoration: InputDecoration(
                       filled: true,
 
-                      fillColor:
-                          const Color(
-                        0xffF9F4FC,
-                      ),
+                      fillColor: const Color(0xffF9F4FC),
 
-                      hintText:
-                          'Username',
+                      hintText: 'Username',
 
-                      prefixIcon:
-                          const Icon(
+                      prefixIcon: const Icon(
                         Icons.person,
-                        color:
-                            Color(
-                          0xffBFA2DB,
-                        ),
+                        color: Color(0xffBFA2DB),
                       ),
 
-                      border:
-                          OutlineInputBorder(
-                        borderRadius:
-                            BorderRadius.circular(
-                          20,
-                        ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
 
-                        borderSide:
-                            BorderSide.none,
+                        borderSide: BorderSide.none,
                       ),
                     ),
                   ),
@@ -230,42 +154,26 @@ class _LoginScreenState
                   const SizedBox(height: 20),
 
                   TextField(
-                    controller:
-                        passwordController,
+                    controller: passwordController,
 
                     obscureText: true,
 
-                    decoration:
-                        InputDecoration(
-
+                    decoration: InputDecoration(
                       filled: true,
 
-                      fillColor:
-                          const Color(
-                        0xffF9F4FC,
-                      ),
+                      fillColor: const Color(0xffF9F4FC),
 
-                      hintText:
-                          'Password',
+                      hintText: 'Password',
 
-                      prefixIcon:
-                          const Icon(
+                      prefixIcon: const Icon(
                         Icons.lock,
-                        color:
-                            Color(
-                          0xffBFA2DB,
-                        ),
+                        color: Color(0xffBFA2DB),
                       ),
 
-                      border:
-                          OutlineInputBorder(
-                        borderRadius:
-                            BorderRadius.circular(
-                          20,
-                        ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
 
-                        borderSide:
-                            BorderSide.none,
+                        borderSide: BorderSide.none,
                       ),
                     ),
                   ),
@@ -277,21 +185,16 @@ class _LoginScreenState
                     height: 55,
 
                     child: ElevatedButton(
-
                       onPressed: login,
 
                       child: isLoading
-                          ? const CircularProgressIndicator(
-                              color:
-                                  Colors.white,
-                            )
+                          ? const CircularProgressIndicator(color: Colors.white)
                           : const Text(
                               'LOGIN',
 
                               style: TextStyle(
                                 fontSize: 16,
-                                fontWeight:
-                                    FontWeight.bold,
+                                fontWeight: FontWeight.bold,
                               ),
                             ),
                     ),
